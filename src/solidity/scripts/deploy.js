a8ef59892594aa4ad1f7cf83b837704d7c6eb3fd
Luis Lucena <lucena@puller.mx>
2023-01-04 19:41:11 +0100

    add script and test

---
+++ src/solidity/scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    // Get the ContractFactories and Signers here.
+    const NFT = await ethers.getContractFactory("NFT");
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    // deploy contracts
+    const marketplace = await Marketplace.deploy(1);
+    const nft = await NFT.deploy();
+  }
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
