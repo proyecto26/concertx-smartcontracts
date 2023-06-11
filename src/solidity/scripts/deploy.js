8fecb4f48151ab1d83ede11cfbc5eb4ef08ac3a2
Luis Lucena <lucena@puller.mx>
2023-06-11 23:26:14 +0200

    update deploy file with addresses

--- src/solidity/scripts/deploy.js
+++ src/solidity/scripts/deploy.js
@@ -7,9 +7,12 @@ async function main() {
     // Get the ContractFactories and Signers here.
     const NFT = await ethers.getContractFactory("NFT");
     const Marketplace = await ethers.getContractFactory("Marketplace");
-    // deploy contracts
+    // Deploy contracts
     const marketplace = await Marketplace.deploy(1);
     const nft = await NFT.deploy();
+
+    console.log("NFT deployed at:", nft.address);
+    console.log("Marketplace deployed at:", marketplace.address);
   }
   main()
     .then(() => process.exit(0))
